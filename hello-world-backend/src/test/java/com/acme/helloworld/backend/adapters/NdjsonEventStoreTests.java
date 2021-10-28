35463656c4dc7d6a9a7e279e682e6c2bd680b09b
Falko Schumann <falko.schumann@muspellheim.de>
2021-10-28 21:02:19 +0200

    Stabilisiere Test für verschiedene Zeitzonen der Build-Umgebung

--- hello-world-backend/src/test/java/com/acme/helloworld/backend/adapters/NdjsonEventStoreTests.java
+++ hello-world-backend/src/test/java/com/acme/helloworld/backend/adapters/NdjsonEventStoreTests.java
@@ -12,7 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
@@ -61,11 +61,11 @@ public class NdjsonEventStoreTests {
     return List.of(
         new UserCreatedEvent(
             "a7caf1b0-886e-406f-8fbc-71da9f34714e",
-            LocalDateTime.of(2021, 7, 23, 21, 54).atZone(ZoneId.systemDefault()).toInstant(),
+            LocalDateTime.of(2021, 7, 23, 19, 54).atZone(ZoneOffset.UTC).toInstant(),
             "Alice"),
         new UserCreatedEvent(
             "d5abc0dd-60b0-4a3b-9b2f-8b02005fb256",
-            LocalDateTime.of(2021, 7, 23, 21, 57).atZone(ZoneId.systemDefault()).toInstant(),
+            LocalDateTime.of(2021, 7, 23, 19, 57).atZone(ZoneOffset.UTC).toInstant(),
             "Bob"));
   }
 }
