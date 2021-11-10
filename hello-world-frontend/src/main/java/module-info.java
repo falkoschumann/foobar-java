1a551ec23b535ab40202218c9024e909cf325d54
Falko Schumann <falko.schumann@muspellheim.de>
2021-11-10 22:17:52 +0100

    Entferne unnötige Abhängigkeit

--- hello-world-frontend/src/main/java/module-info.java
+++ hello-world-frontend/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.acme.helloworld.frontend {
   requires javafx.controls;
   requires javafx.fxml;
   requires jdk.localedata;
-  requires static lombok;
   requires transitive com.acme.helloworld.contract;
 
   exports com.acme.helloworld.frontend;
